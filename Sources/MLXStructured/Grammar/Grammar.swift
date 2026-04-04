c844dfd25c40a87dac121ec4b8b5e4ef63884b80
petrukha-ivan <petrukha@macpaw.com>
2026-04-04 15:09:18 +0200

    Remove deprecated code

--- Sources/MLXStructured/Grammar/Grammar.swift
+++ Sources/MLXStructured/Grammar/Grammar.swift
@@ -14,34 +14,3 @@ public enum Grammar {
     case schema(String, options: JSONSchemaFormatOptions = .init())
     case structural(String)
 }
-
-public extension Grammar {
-
-    @available(*, deprecated, message: "Prefer constructing prompt manually, this property will be removed in the future versions")
-    var raw: String {
-        switch self {
-        case .ebnf(let ebnf):
-            return ebnf
-        case .regex(let regex):
-            return regex
-        case .schema(let schema, _):
-            return schema
-        case .structural(let tag):
-            return tag
-        }
-    }
-
-    @available(*, deprecated, message: "Prefer constructing prompt manually, this property will be removed in the future versions")
-    var guidance: String? {
-        switch self {
-        case .ebnf:
-            return nil
-        case .regex(let regex):
-            return "Output is regex constrained: \(regex)"
-        case .schema(let schema, _):
-            return "Output is JSON schema constrained: \(schema)"
-        case .structural:
-            return nil
-        }
-    }
-}
